c2d12258f8458ea8cf6fd5c7f54cea700eba1691
Mintraaa1 <654259019@webmail.npru.ac.th>
2025-08-25 17:14:09 +0700

    TodayServer

--- server/routes/restaurant.router.js
+++ server/routes/restaurant.router.js
@@ -1,21 +1,32 @@
 import restaurantController from "../controllers/restaurant.controller.js";
+import authMiddleware from "../middleware/authJWT.js";
 
 import express from "express";
 const router = express.Router();
 
 //POST http://localhost:5000/api/v1/restaurants
-router.post("/", restaurantController.create);
+router.post("/", authMiddleware.verifyToken,
+    authMiddleware.isModOrAdmin,
+    restaurantController.create
+);// ต้องวางตามลำดับนี้ เพราะถ้าวางหลัง /:id จะทำให้ route นี้ไม่ทำงาน
 
 //GET http://localhost:5000/api/v1/restaurants
-router.get("/", restaurantController.getAll);
+router.get("/", authMiddleware.verifyToken,
+    restaurantController.getAll);
 
 //GET http://localhost:5000/api/v1/restaurants/:id
-router.get("/:id", restaurantController.getById);
+router.get("/:id", authMiddleware.verifyToken,
+    restaurantController.getById);
 
 //PUT http://localhost:5000/api/v1/restaurants/:id
-router.put("/:id", restaurantController.update);
+router.put("/:id", authMiddleware.verifyToken,
+    authMiddleware.isModOrAdmin,
+    restaurantController.update
+);
 
 //DELETE http://localhost:5000/api/v1/restaurants/:id
-router.delete("/:id", restaurantController.deleteById);
+router.delete("/:id", authMiddleware.verifyToken,
+    authMiddleware.isModOrAdmin,
+    restaurantController.deleteById);
 
 export default router;

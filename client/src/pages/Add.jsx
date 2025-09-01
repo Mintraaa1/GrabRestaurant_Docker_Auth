ed4706800f404e968f1c7a8dd278ddfe5ce59c7f
Mintraaa1 <654259019@webmail.npru.ac.th>
2025-09-01 15:54:24 +0700

    fix: correct Navbar import path

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import NavBar from "../components/NavBar.jsx";
+import Navbar from "./components/Navbar.jsx";
 import RestaurantService from "../services/restaurtant.service.js";
 import Swal from "sweetalert2";
 const Add = () => {

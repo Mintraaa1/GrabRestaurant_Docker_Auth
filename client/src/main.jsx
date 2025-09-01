ca8299128df1356d6aa307d5637c883f56a3979e
Mintraaa1 <654259019@webmail.npru.ac.th>
2025-09-01 16:15:43 +0700

    vercel render2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,7 +9,7 @@ import { AuthProvider } from "./context/AuthContext";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <NavBar />
+      <Navbar />
       <RouterProvider router={router} />
     </AuthProvider>
   </StrictMode>

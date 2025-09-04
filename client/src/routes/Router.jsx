cb3d97f63715dea443d394e0d4289dec8b570938
Mintraaa1 <654259019@webmail.npru.ac.th>
2025-09-04 11:08:07 +0700

    Routrr

--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -7,7 +7,7 @@ import Register from "../pages/Register";
 import NotAllowed from "../pages/NotAllowed";
 import AdminPage from "../pages/AdminPage";
 import UserPage from "../pages/UserPage";
-import ModOrAdmin from "../pages/ModOrAdminPage";
+import ModOrAdmin from "../pages/ModOrAdmin";
 import Profile from "../pages/Profile";
 const router = createBrowserRouter([
   {
